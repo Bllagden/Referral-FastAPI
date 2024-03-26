67f6da1c44d1ed655e6495b902d19e3a2173249e
Bllagden <bllagden99@gmail.com>
2024-03-26 21:44:00 +0400

    Del old api and dao

--- src/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from .ref_codes.router import router_ref_codes
-from .users.router import router_users, router_users_auth
-
-__all__ = [
-    "router_ref_codes",
-    "router_users",
-    "router_users_auth",
-]
--- src/dao/referrals_dao.py
+++
@@ -1,7 +0,0 @@
-from db.models import Referrals
-
-from .base_dao import BaseDAO
-
-
-class ReferralsDAO(BaseDAO):
-    model = Referrals
